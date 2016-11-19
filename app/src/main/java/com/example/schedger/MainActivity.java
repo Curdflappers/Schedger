47ab705130108f50acbf6713776724fdc935ee8a
Payton Garland <pgarland2@wisc.edu>
2016-11-19 09:17:08 -0600

    Added border and onClick listener for tasks list

--- app/src/main/java/com/example/schedger/MainActivity.java
+++ app/src/main/java/com/example/schedger/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.schedger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static com.example.schedger.R.color.red;
+import static com.example.schedger.R.drawable.border;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout tasks = (LinearLayout )findViewById(R.id.tasks);
+        tasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tasks_intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(tasks_intent );
+            }
+        });
+
+        LinearLayout events = (LinearLayout )findViewById(R.id.events);
 
 
         Task task1 = new Task("test", 1, 1, 1);
@@ -23,18 +38,15 @@ public class MainActivity extends AppCompatActivity {
         Task task7 = new Task("test3", 1, 1, 1);
         Task task8 = new Task("test4", 1, 1, 1);
 
-        LinearLayout tasks = (LinearLayout)findViewById(R.id.tasks);
         for( int i = 0; i < Planner.tasks.size(); i++ )
         {
             TextView textView1 = new TextView(this);
-            TextView textView2 = new TextView(this);
-            textView1.setText(Planner.tasks.get(i).getName());
-            textView2.setText("Due in: " + Planner.tasks.get(i).getDuration());
+            textView1.setText(Planner.tasks.get(i).getName() + "\n" + "Due in: " + Planner.tasks.get(i).getDuration());
+            textView1.setBackgroundResource(R.drawable.border);
+            textView1.setPadding(10, 10, 10, 10);
             tasks.addView(textView1);
-            tasks.addView(textView2);
         }
 
-        LinearLayout events = (LinearLayout)findViewById(R.id.events);
         for( int i = 0; i < Planner.tasks.size(); i++ )
         {
             TextView textView = new TextView(this);
