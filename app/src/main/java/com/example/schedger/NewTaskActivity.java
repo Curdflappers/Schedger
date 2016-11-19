47607c2a530c269de3336868913577a3132fd424
Mark Wiemer <mwwiemer@outlook.com>
2016-11-19 10:40:14 -0600

    updated ids of views

--- app/src/main/java/com/example/schedger/NewTaskActivity.java
+++ app/src/main/java/com/example/schedger/NewTaskActivity.java
@@ -2,6 +2,7 @@ package com.example.schedger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTaskActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class NewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+    }
+
+    static void saveTask(View v)
+    {
+        // validate task
+            // show dialog box if invalid
+            // quit method
+        // save to planner
+        // return to previous screen
+        // display "task saved" notification
     }
 }
